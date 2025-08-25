56047a605b841645a016120aa6751d020cc0bd5a
Rajan Jha <rajanj448@gmail.com>
2025-08-25 22:44:29 +0530

    nest database and validator setup

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,9 +1,21 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import * as Joi from '@hapi/joi';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      
+      isGlobal: true,
+      validationSchema:Joi.object({
+        DATABASE_URL: Joi.string().required(),
+      }),
+    }),
+    MongooseModule.forRoot(process.env.DATABASE_URL!),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
--- server/src/main.ts
+++ server/src/main.ts
@@ -1,8 +1,20 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as passport from 'passport';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  app.use(passport.initialize());
+   app.useGlobalPipes(new ValidationPipe(
+      {
+        whitelist: true,
+        transform: true,
+        transformOptions: {
+          enableImplicitConversion: true
+        }
+      }
+  ));
+  await app.listen(5000);
 }
 bootstrap();
